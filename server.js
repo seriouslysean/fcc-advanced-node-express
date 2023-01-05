33d9db4431e8de5921866e580ef8920029495ed2
Sean Kennedy <skennedy@urbn.com>
2023-01-05 12:01:09 -0500

    Add profile page

--- server.js
+++ server.js
@@ -3,19 +3,80 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-  return res.render('index');
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+    });
+  });
+  app.route('/profile').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('profile');
+  });
+
+  app.route('/login').post(
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (res) => res.redirect('/profile'),
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  }));
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: e,
+      message: 'Unable to connect to database',
+    });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
